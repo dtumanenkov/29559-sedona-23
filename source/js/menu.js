675b1b4159779985b463068f1793b02528ed4b22
dTum <50366412+dtumanenkov@users.noreply.github.com>
2021-08-29 20:06:07 +0300

    29_08_JS

--- source/js/menu.js
+++ source/js/menu.js
@@ -4,19 +4,13 @@ const menu_close=document.querySelector(".main-nav__toggle--close");
 
 menu_link.addEventListener("click",function(evt){
   evt.preventDefault();
-  menu_popup.classList.remove("hidden");
+  menu_popup.classList.remove("visually-hidden");
+  menu_close.classList.remove("visually-hidden");
 });
 
 menu_close.addEventListener("click",function(evt){
   evt.preventDefault();
-  menu_popup.classList.add("hidden");
+  menu_popup.classList.add("visually-hidden");
+  menu_close.classList.add("visually-hidden");
 });
 
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (!menu_link.classList.contains("hidden")) {
-      evt.preventDefault();
-      mapPopup.classList.add("hidden");
-    }
-  }
-});
