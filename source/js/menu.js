a874bf9da86905a7e75c8b6152c6b7868b754877
dTum <50366412+dtumanenkov@users.noreply.github.com>
2021-09-04 22:34:00 +0300

    noJS

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,6 +1,9 @@
 const menu_link=document.querySelector(".main-nav__toggle");
 const menu_popup=document.querySelector(".site-list");
 const menu_close=document.querySelector(".main-nav__toggle--close");
+const no_js=document.querySelector(".no-js");
+
+menu_popup.classList.remove("site-list--nojs");
 
 menu_link.addEventListener("click",function(evt){
   evt.preventDefault();
