3042c4c6f422513210a4aa9dff08f4f4f7e9118c
dTum <skyline400@gmail.com>
2021-09-06 15:13:05 +0300

    06_09_nojs

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,8 +1,15 @@
 const menu_link=document.querySelector(".main-nav__toggle");
 const menu_popup=document.querySelector(".site-list");
 const menu_close=document.querySelector(".main-nav__toggle--close");
+const logo=document.querySelector(".page-header__logo");
+const nav=document.querySelector(".page-header__nav");
+const greeting=document.querySelector(".page-header__greeting");
+const heading=document.querySelector(".page-header__heading");
 
 menu_popup.classList.remove("site-list--nojs");
+logo.classList.remove("page-header__logo--nojs");
+nav.classList.remove("page-header__nav--nojs");
+greeting.classList.remove("page-header__greeting--nojs");
 
 menu_link.addEventListener("click",function(evt){
   evt.preventDefault();
